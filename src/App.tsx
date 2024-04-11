fda538c5c733caf750e53e242ed5649412ad0c11
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-04-11 11:42:26 +0200

    0.0.1 Added index.html Added main.tsx Added App.tsx Added App.css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import ArtistCard from "./components/ArtistCard/ArtistCard";
+import CUESheetButton from "./components/Buttons/CUE_Sheet_Button/CUE_Sheet_Button";
+import SearchBar from "./components/SearchBar/Search_Bar";
+import InformationLabel from "./components/InformationLabel/Information_Label";
+
+function App() {
+
+    return <ArtistCard/> ;
+}
+
+export default App;
