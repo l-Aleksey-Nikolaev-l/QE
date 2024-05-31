c31dc40926e20a85e2ba1f3c6a9fd3d2ea8fd7bb
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-05-31 17:45:33 +0200

    0.0.9 Changed: App, Alert, Button and ListGroup tsx files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
+import Button from "./FOR_TEST/Button";
+import Alert from "./FOR_TEST/Alert";
+
 import ArtistCard from "./components/ArtistCard/ArtistCard";
 import CUESheetButton from "./components/Buttons/CUE_Sheet_Button/CUE_Sheet_Button";
 import SearchBar from "./components/SearchBar/Search_Bar";
 import InformationLabel from "./components/InformationLabel/Information_Label";
+import { useState } from "react";
 
 function App() {
 
-    return <ArtistCard/> ;
+    // const [alertVisible, setAlertVisibility] = useState(false);
+    return (
+        <div>
+            {/*<Button onClick={() => setAlertVisibility(true)}></Button>*/}
+            {/*{alertVisible && <Alert onClose={() => setAlertVisibility(false)}></Alert>}*/}
+            <SearchBar></SearchBar>
+            <ArtistCard></ArtistCard>
+        </div>
+    );
 }
 
 export default App;
